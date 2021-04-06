3f35f5af53ebcb78faf2bc2824526b0ec8a8c5b0
akira-vishal <vishal.rockstar7011@gmail.com>
2021-04-06 17:38:23 +0700

    build: use clang

--- build.sh
+++ build.sh
@@ -24,8 +24,8 @@ export USE_CCACHE=1
 #
 #ccache variables
 export CCACHE_DIR="$HOME/.ccache"
-export CC="ccache gcc"
-export CXX="ccache g++"
+export CC="ccache clang"
+export CXX="ccache clang++"
 export PATH="/usr/lib/ccache:$PATH"
 #
 #Export variables
@@ -33,7 +33,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
 export LOCALVERSION="-Z2"
-export KBUILD_COMPILER_STRING=$(gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+export KBUILD_COMPILER_STRING=$(clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 export KBUILD_BUILD_HOST="vishal"
 export KBUILD_BUILD_USER="akira"
 export CROSS_COMPILE=aarch64-linux-gnu-
@@ -42,17 +42,13 @@ export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 function checker() {
     if [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ]
        then
-        echo -e ""
         echo -e "$CYAN- Cloning AnyKernel3$WHITE"
         cd && git clone https://github.com/akira-vishal/AnyKernel3.git AnyKernel
-        echo -e ""
         echo -e "$GREEN- Done!$WHITE"
         cd $HOME && cd $KERNEL_DIR
         zipper
        else
-        echo -e ""
-        echo -e "$RED- Build Failed$WHITE"
-        
+        echo -e "$RED- Build Failed$WHITE"    
     fi
 }
 #
@@ -60,7 +56,6 @@ function zipper() {
     rm -f $HOME/AnyKernel/Image.gz*
     rm -f $HOME/AnyKernel/zImage*
     rm -f $HOME/AnyKernel/dtb*
-    echo -e ""
     echo -e "$CYAN- Time To ZIP Up!$WHITE"
     cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $HOME/AnyKernel
     cd $HOME/AnyKernel || exit 1
@@ -68,16 +63,12 @@ function zipper() {
     cd $HOME && cd $KERNEL_DIR
     END=$(date +"%s")
     DIFF=$(($END - $START))
-    echo -e ""
 	echo -e "$GREEN- Build Completed Succesfully$WHITE"
-	echo -e ""
 	echo -e "$GREEN- Build took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s).$WHITE"
-	echo -e ""
 }
 #
 function compiler() {
     rm -rf $KERNEL_DIR/out
-    echo -e ""
 	echo -e "$CYAN- Building Kernel$WHITE"
     make clean && make mrproper O=out
     make O=out	${DEFCONFIG}
